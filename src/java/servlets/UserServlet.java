76a6319eac9eb6b2130069bc326743c5191847ca
855474 <855474@SAIT227642.ACDM.DS.SAIT.CA>
2021-07-10 19:21:36 -0600

    Fifth Commit ~ Added proper CSS to application

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -19,16 +19,21 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        // create session object
+        HttpSession session = request.getSession();
         // create user service object
         UserService userService = new UserService();
+        // create List object to store user information
         List<User> userList = null;
+        
         try {
+            // retrieve all users in the DB
             userList = userService.getAll();
+            // display the users to the main page
+            request.setAttribute("userList", userList);
         } catch (Exception ex) {
             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        request.setAttribute("userList", userList);
         // Display the main page
         getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
     }
@@ -36,7 +41,55 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Display the main page
-        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
+
+        // create user service object
+        UserService userService = new UserService();
+
+        // grabs the actions available in the application
+        String action = request.getParameter("action");
+
+        try {
+            switch (action) {
+                case "add":
+                    // grabs the email from the input field
+                    String email = request.getParameter("input_email");
+                    // grabs the email from the input field
+                    String firstName = request.getParameter("input_firstName");
+                    // grabs the email from the input field
+                    String lastName = request.getParameter("input_lastName");
+                    // grabs the password from the input field
+                    String password = request.getParameter("input_password");
+                    // grabs the role from the dropdown list
+                    int role = Integer.parseInt(request.getParameter("select_role"));
+                    // grabs the isActive from the checkbox
+                    String isActive = request.getParameter("input_active");
+                    boolean activeStatus = true;
+                    // checks if user has entered all the necessary information
+                    if (email != null || !email.equals("") && firstName != null || !firstName.equals("") && lastName != null || !lastName.equals("") && password != null || !password.equals("")) {
+                        if (isActive == null || isActive.equals("")) {
+                            activeStatus = false;
+                        }
+
+                        try {
+                            userService.insert(email, activeStatus, firstName, lastName, password, role);
+                        } catch (Exception ex) {
+                            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    request.setAttribute("test_message", "works"); //DEBUG
+                    break;
+
+                case "update":
+                    break;
+
+                case "delete":
+                    break;
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // send the user to the main page
+        response.sendRedirect("UserServlet");
     }
 }
--- web/WEB-INF/users.jsp
+++ web/WEB-INF/users.jsp
@@ -1,9 +1,3 @@
-<%-- 
-    Document   : users
-    Created on : Jul 5, 2021, 8:08:56 PM
-    Author     : 855474
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
@@ -14,35 +8,40 @@
         <title>User DB Management System</title>
     </head>
     <body>
-        <div>
-            <h3>Add User</h3>
-            <input type="text" placeholder="Email" name="input_email" value=""><br>
-            <input type="text" placeholder="First Name" name="input_firstName" value=""><br>
-            <input type="text" placeholder="Last Name" name="input_lastName" value=""><br>
-            <input type="text" placeholder="Password" name="input_password" value=""><br>
+        <div class="input_fields" id="add-wrapper">
+            <form action="" method="post">
+                <h3>Add User</h3>
+                <input type="text" placeholder="Email" name="input_email" value=""><br>
+                <input type="text" placeholder="First Name" name="input_firstName" value=""><br>
+                <input type="text" placeholder="Last Name" name="input_lastName" value=""><br>
+                <input type="text" placeholder="Password" name="input_password" value=""><br>
+                <select name="select_role">
+                    <option value="1">System Admin</option>
+                    <option value="2">Regular User</option>
+                    <option value="3">Company Admin</option>
+                </select> <br>
+                <p style="display:inline-block;">Active?</p>
+                <input type="checkbox" name="input_active" value=""><br>
+                <input type="submit" name="save_Button" value="Save">
+                <input type="hidden" name="action" value="add">
+            </form>
+        </div>
+
+        <div class="input_fields" id="edit-wrapper">
+            <h3 >Edit User</h3>
+            <input type="text" placeholder="First Name" name="edit_firstName" value=""><br>
+            <input type="text" placeholder="Last Name" name="edit_lastName" value=""><br>
+            <input type="text" placeholder="Password" name="edit_password" value=""><br>
             <select>
                 <option value="">System Admin</option>
                 <option value="">Regular User</option>
                 <option value="">Company Admin</option>
             </select> <br>
-            <input type="checkbox" name="input_active" value=""><br>
             <input type="submit" name="save_Button" value="Save">
         </div>
 
-        <div>
-            <h3>Edit User</h3>
-            <input type="text" placeholder="First Name" name="input_firstName" value=""><br>
-            <input type="text" placeholder="Last Name" name="input_lastName" value=""><br>
-            <input type="text" placeholder="Password" name="input_password" value=""><br>
-            <select>
-                <option value="">System Admin</option>
-                <option value="">Regular User</option>
-                <option value="">Company Admin</option>
-            </select> <br>
-            <input type="submit" name="save_Button" value="Save">
-        </div>
-
-        <div>
+        <div class="view_database">
+            <h3 id="view-header">View Users</h3>
             <table>
                 <tr>
                     <th>Email</th>
@@ -60,5 +59,7 @@
                 </c:forEach>
             </table>
         </div>
+        
+        <p>${test_message}</p>
     </body>
 </html>
